0d33d3cb2b2744b9802a5f2695cf5d0638623096
Jordan Ambra <jordan@serenity.software>
2020-07-01 23:03:42 -0400

    Add version and environment to configuration

--- paymentech/__init__.py
+++ paymentech/__init__.py
@@ -1,15 +1,28 @@
+import pathlib
+
+
 configuration = {
     "merchant_id": None,
     "bin": None,
     "username": None,
-    "password": None
+    "password": None,
+    "environment": "test",
+    "version": ""
 }
 
 
-def configure(merchant_id, bin, username, password, **kwargs):
+def configure(merchant_id, bin, username, password, environment="test", **kwargs):
     configuration["merchant_id"] = merchant_id
     configuration["bin"] = bin
     configuration["username"] = username
     configuration["password"] = password
+    configuration["environment"] = environment
+
+    if environment not in ["test", "production"]:
+        raise ValueError("No environment named {0}".format(environment))
+
+    version_file_path = pathlib.Path(__file__).parent.parent.absolute() / "VERSION"
+    with open(version_file_path, "r") as version_file:
+        configuration["version"] = version_file.read().strip()
 
     configuration.update(kwargs)
