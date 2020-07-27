68e8320e63dd91cfe2da62431deedc2151bef579
Jordan Ambra <jordan@serenity.software>
2020-07-26 22:03:49 -0400

    Add common exception

---
+++ paymentech/exceptions.py
@@ -0,0 +1,8 @@
+class PaymentechException(Exception):
+
+    def __init__(self, message, code=None):
+        self.message = message
+        self.code = code or 0
+
+    def __str__(self):
+        return f"Error {self.code}: {self.message}"
